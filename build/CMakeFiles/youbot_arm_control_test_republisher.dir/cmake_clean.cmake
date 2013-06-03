98ee0112c0005c1be6cfb4f1e741f77ec4945e71
Adam Jardim <adamj2010@wpi.edu>
2013-06-03 12:04:17 -0400

    Added tele-op controls for the arm

---
+++ build/CMakeFiles/youbot_arm_control_test_republisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/youbot_arm_control_test_republisher.dir/examples/youbot_arm_control_test_republisher.cpp.o"
+  "../bin/youbot_arm_control_test_republisher.pdb"
+  "../bin/youbot_arm_control_test_republisher"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/youbot_arm_control_test_republisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/youbot_arm_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/youbot_arm_test.dir/examples/youbot_arm_test.cpp.o"
+  "../bin/youbot_arm_test.pdb"
+  "../bin/youbot_arm_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/youbot_arm_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
